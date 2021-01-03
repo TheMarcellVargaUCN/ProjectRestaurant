a8e76e0c65d7de8ea3ebdc706f14ac6d0ac4148d
DominikRostek <1089269@ucn.dk>
2021-01-03 13:48:21 +0100

    Update java.js

--- assets/css/java.js
+++ assets/css/java.js
@@ -1,16 +1,7 @@
 const elmBox = document.querySelector ("#box");
 const elmMenu = document.querySelector ("#menu");
 
-document.querySelector("#menu")
-    .addEventListener ("click", function (){
-        elmBox.classList.toggle ("show")
-    });
-
-
-// function toggle_visibility(#menu) {
-//         var e = document.getElementById(#menu);
-//         if(e.style.display == 'block')
-//            e.style.display = 'none';
-//         else
-//            e.style.display = 'block';
-//      }
+ document.querySelector("#menu")
+     .addEventListener ("click", function (){
+         elmBox.style.display = 'grid';
+     });
